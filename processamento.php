2ed158d760482b25665f6d681bc8e60f86fbd15e
MarquesdevOn <thalitajasse@gmail.com>
2024-10-30 17:20:49 -0300

    updates do formulário

--- processamento.php
+++ processamento.php
@@ -17,14 +17,36 @@ $nome = $_POST["nome"];
 $email = $_POST["email"];
 $nascimento = $_POST["nascimento"];
 $mensagem = $_POST["mensagem"];
+
+// Se um dos campos obrigátorios estiver vazio
+if( empty ($nome) || empty($email) ){
 ?>
 
+    <!-- mostramos um alerta para o usuário -->
+    <p style="color: red;">VOCÊ DEVE PREENCHER <b>nome</b> e <b>e-mail</b></p>
+<?php
+} else{
+?>
     <h2>Dados recebidos</h2>
     <ul>
         <li>Nome:  <?=$nome?></li>
+
         <li>E-mail:  <?=$email?></li>
+
+        <!-- Verificando se os campos nascimento e mensagem NAO ESTAO VAZIOS
+        Note o uso do operador ! para inverter a lógica da funcao empty() -->
+        <?php if( !empty($nascimento)){ ?>
         <li>Nascimento:  <?=$nascimento?></li>
+        <?php }?>
+
+        <?php if( !empty($mensagem)){ ?>
         <li>Mensagem:  <?=$mensagem?></li>
+        <?php } ?>
+
+        
+
     </ul>
+
+<?php } ?>
 </body>
 </html>
